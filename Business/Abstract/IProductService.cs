ca5754486f0ca7efeb1a23c03ff8f7ff8e1481d1
Rumeysa Özdemir <rumeysa_2789@hotmail.com>
2023-04-08 22:49:50 +0300

    Add project files.

---
+++ Business/Abstract/IProductService.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface IProductService
+    {
+        Product GetById(int productId);
+        List<Product> GetList();
+        List<Product> GetListByCategory(int categoryId);
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(Product product);
+
+    }
+}
