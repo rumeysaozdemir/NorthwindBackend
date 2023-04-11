3aa2fbaeadac44cc9b4453094401258d3d195a53
Rumeysa Özdemir <rumeysa_2789@hotmail.com>
2023-04-11 22:34:57 +0300

    11.04.2023

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using Entities.Concrete;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -26,5 +27,64 @@ namespace WebAPI.Controllers
             }
             return BadRequest(result.Message);
         }
-    }
+
+        [HttpGet("getlistbycategory")]
+        public IActionResult GetListByCategory(int categoryId)
+        {
+            var result = _productService.GetListByCategory(categoryId);
+            if (result.Success)
+            {
+                return Ok(result.Data);
+
+            }
+            return BadRequest(result.Message);
+        }
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int productId)
+        {
+            var result = _productService.GetById(productId);
+            if (result.Success)
+            {
+                return Ok(result.Data);
+
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Product product)
+        {
+           var result =  _productService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("update")] //
+        public IActionResult Update(Product product) 
+        {
+            var result = _productService.Update(product);
+            if (result.Success) 
+            {
+                return Ok(result.Message);
+            }
+            return BadRequest(result.Message);
+      
+        }
+        [HttpPost("delete")] //
+        public IActionResult Delete(Product product)
+        {
+            var result = _productService.Delete(product);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+            return BadRequest(result.Message);
+
+        }
+
+    } 
 }
