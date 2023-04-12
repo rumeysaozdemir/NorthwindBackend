7b3c54ab1d2a274183e31e95fc25c26caa103974
Rumeysa Özdemir <rumeysa_2789@hotmail.com>
2023-04-12 22:29:15 +0300

    12.04.2023

---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,27 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        ICategoryDal _categoryDal;
+
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+
+        public IDataResult<List<Category>> GetList()
+        {
+            return new SuccessDataResult<List<Category>>(_categoryDal.GetList().ToList());
+        }
+    }
+}
