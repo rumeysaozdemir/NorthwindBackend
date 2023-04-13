ebd91db4f930c8a47bfea95000a016e96774b601
Rumeysa Özdemir <rumeysa_2789@hotmail.com>
2023-04-13 23:03:58 +0300

    13.04.2023

---
+++ DataAccess/Concrete/EntityFramework/EfUserDal.cs
@@ -0,0 +1,29 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework.Contexts;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfUserDal : EfEntityRepositoryBase<User, NorthwindContext>, IUserDal
+    {
+        List<OperationClaim> IUserDal.GetClaims(User user)
+        {
+            using (var context = new NorthwindContext())
+            {
+                //burada join işlemi yapıyoruz
+
+                var result = from OperationClaim in context.UserOperationClaims
+                             join UserOperationClaim in context.UserOperationClaims
+                             on OperationClaim.Id equals UserOperationClaim.OperationClaimId
+                             where UserOperationClaim.UserId == User.Id
+                             select new OperationClaim { Id = OperationClaim.Id, Name = OperationClaim.Name };
+                return result.ToList();
+            }
+        }
+    }
+}
+
