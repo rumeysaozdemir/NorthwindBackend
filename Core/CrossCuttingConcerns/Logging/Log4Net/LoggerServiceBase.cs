9004c609f7037e8b744c2acf6264b054e7894d49
Rumeysa Özdemir <rumeysa_2789@hotmail.com>
2023-05-17 22:07:53 +0300

    17.05.2023

---
+++ Core/CrossCuttingConcerns/Logging/Log4Net/LoggerServiceBase.cs
@@ -0,0 +1,26 @@
+﻿using log4net;
+using log4net.Repository;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Xml;
+
+namespace Core.CrossCuttingConcerns.Logging.Log4Net
+{
+    public class LoggerServiceBase
+    {
+        private readonly ILog _log;
+        public LoggerServiceBase(string name)
+        {
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.Load(File.OpenRead("log4net.config"));
+            
+            ILoggerRepository loggerRepository = LogManager.CreateRepository(Assembly.GetEntryAssembly(),typeof(log4net.Repository.Hierarchy.Hierarchy));
+            log4net.Config.XmlConfigurator.Configure(loggerRepository, xmlDocument["log4net"]);
+
+            _log = LogManager.GetLogger(loggerRepository.Name, name);
+         }
+    }
+}
